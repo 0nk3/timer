91886f16fe255456449a57d974f60147abbe896f
0nk3 <0nk3.0nk3@gmail.com>
2020-08-31 16:10:45 +0200

    add ioic circular progress bar to project

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,34 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+import { IonicModule } from '@ionic/angular';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    // Specify ng-circle-progress as an import
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+      animation: false,
+      responsive: true,
+      renderOnClick: false,
+      // backgroundStroke: "transparent",
+      // clockwise: false,
+    }),
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
